70f5327098a6dd31fbf7762ff5906b56d52bea0e
Marike <marike.puranen@laura.fi>
2024-09-08 22:14:58 +0300

    Set up database + add schema

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
-from flask import render_template
+from os import getenv
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute(text("SELECT username FROM users"))
+    users = result.fetchall()
+    counter = len(users)
+    return render_template("index.html", count=counter)
