201fd4bceae6321c23006fcf6aaff11531261630
Marike <marike.puranen@laura.fi>
2024-09-26 19:42:11 +0300

    Small improvements based on comments received in reviews

--- register.py
+++ register.py
@@ -8,17 +8,25 @@ def add_user():
     password = request.form["password"]
     confirm_password = request.form["confirm_password"]
 
+    errors = []
+
+    if " " in username:
+        errors.append('Username cannot contain spaces.')
+
     check_username = db.session.execute(text(
     "SELECT username FROM users WHERE username = :username"),
     {"username": username})
     existing_user = check_username.fetchone()
 
     if existing_user:
-        flash('Username already in use. Please choose another one.', 'danger')
-        return redirect("/")
+        errors.append('Username already in use. Please choose another one.')
 
     if password != confirm_password:
-        flash('Passwords do not match!', 'danger')
+        errors.append('Passwords do not match!')
+
+    if errors:
+        for error in errors:
+            flash(error, 'danger')
         return redirect("/")
 
     hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
@@ -26,5 +34,7 @@ def add_user():
     sql = text("INSERT INTO users (username, password, role) VALUES (:username, :password, :role)")
     db.session.execute(sql, {"username":username, "password":hashed_password, "role":role})
     db.session.commit()
+
+    flash('Registration successful!', 'success')
 
     return True
